87ad4930e0b0c61e20f999736c3a0cb405a3aa14
MrRascal <n.levigne@gmail.com>
2017-08-02 19:54:14 -0600

    Added common.h

---
+++ common.h
@@ -0,0 +1,6 @@
+#define ERROR_LOG(...)                                                  \
+    do{                                                                  \
+        fprintf(stderr, "[E]:%s:%d ", __FILE__, __LINE__);                                         \
+        fprintf(stderr, __VA_ARGS__);                                    \
+        fprintf(stderr, "\n");                                           \
+    } while(0)
